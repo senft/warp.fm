97066095eacb4fc0d66ca56e8bdd856da8123299
senft <senftt@gmail.com>
2012-03-07 20:41:51 +0100

    Implemented get_artist_info() to include artist-urls in stats

--- fetchfm.py
+++ fetchfm.py
@@ -82,32 +82,35 @@ class FetchFM:
 
         # TODO This doesn't get all tracks if the user listened 200+ track that
         # day
-        query = {'method': 'user.getRecentTracks',
-                 'user': username,
-                 'from': self.start,
-                 'to': self.end,
-                 'api_key': self.API_KEY,
-                 'limit': 200,
-                 'format': 'json'}
+        result = self._do_query({'method': 'user.getRecentTracks',
+                                 'user': username,
+                                 'from': self.start,
+                                 'to': self.end,
+                                 'api_key': self.API_KEY,
+                                 'limit': 200,
+                                 'format': 'json'})
 
-        try:
-            # Create an API Request
-            url = self.API_URL + "?" + urllib.urlencode(query)
-
-            # Send Request and Collect it
-            data = urllib2.urlopen(url)
-            response_data = json.load(data)
-
-            result = self._parse_response(response_data)
-
-            # Close connection
-            data.close()
-        except urllib2.HTTPError, e:
-            print "HTTP error: %d" % e.code
-        except urllib2.URLError, e:
-            print "Network error: %s" % e.reason.args[1]
-
+        result = self._parse_response(result)
         return result
+
+    def get_artist_info(self, artistname):
+        if artistname in self._artist_cache:
+            return self._artist_cache[artistname]
+
+        # Else: Fetch from last.fm
+        result = self._do_query({'method': 'artist.getInfo',
+                                 'artist': artistname.encode('utf-8'),
+                                 'api_key': self.API_KEY,
+                                 'format': 'json'})
+
+        artist = {'name': result['artist']['name'],
+                  'url': result['artist']['url'],
+                  'img': result['artist']['image'][0]['#text']}
+
+        # Add artist to cache
+        self._artist_cache[artist['name']] = artist
+
+        return artist
 
     def _parse_response(self, data):
         # TODO Pull try/except block in the for-loop and get rid of the
@@ -122,7 +125,6 @@ class FetchFM:
             result['date'] = self.get_formated_day()
 
             for track in tracks:
-
                 if '@attr' in track and 'nowplaying' in track['@attr']:
                     if track['@attr']['nowplaying'] == 'true':
                         # user.getRecentTracks includes the song currently
@@ -130,6 +132,7 @@ class FetchFM:
                         #print 'Skipped currently playing track: ', track
                         continue
                 result['tracks'].append(self._parse_track(track))
+                break
 
             result['random_tracks'] = self._get_random_tracks(result['tracks'])
             result['top_tracks'] = self._parse_top_tracks(result['tracks'])
@@ -142,7 +145,8 @@ class FetchFM:
         return result
 
     def _get_random_tracks(self, tracks):
-        # If we have 10+ tracks pick 10 random ones, else pick all
+        """ If there are 10+ tracks it returns 10 random picks, else all (sorted
+        by the time played). """
         if len(tracks) <= NUM_RANDOM_TRACKS:
             rtracks = tracks
         else:
@@ -154,51 +158,19 @@ class FetchFM:
         timestamp = float(track['date']['uts'])
         time = datetime.fromtimestamp(timestamp).strftime('%H:%M')
 
-        result = {'artist': track['artist']['#text'],
+        result = {'artist': self.get_artist_info(track['artist']['#text']),
                   'title': track['name'],
                   'album': track['album']['#text'],
                   'url': track['url'],
                   'time': time,
-                  'img_small': track['image'][0]['#text']}
+                  'img': track['image'][0]['#text']}
         return result
-
-    def get_artist_info(self, artist):
-        if artist in self._artist_cache:
-            return self._artist_cache[artist]
-
-        # Else: Fetch from last.fm
-        query = {'method': 'artist.getInfo',
-                 'arrist': artist,
-                 'api_key': self.API_KEY,
-                 'format': 'json'}
-
-        try:
-            # Create an API Request
-            url = self.API_URL + "?" + urllib.urlencode(query)
-
-            # Send Request and Collect it
-            data = urllib2.urlopen(url)
-
-            # Print it
-            response_data = json.load(data)
-
-            result = self._parse_response(response_data)
-
-            #Close connection
-            data.close()
-        except urllib2.HTTPError, e:
-            print "HTTP error: %d" % e.code
-        except urllib2.URLError, e:
-            print "Network error: %s" % e.reason.args[1]
-
-    def _parse_artist(self, artist):
-
 
 
     def _parse_top_tracks(self, tracks):
         count = {}
         for track in tracks:
-            title = ''.join([track['artist'], ' - ',
+            title = ''.join([track['artist']['name'], ' - ',
                                    track['title']])
             if title in count:
                 count[title]['count'] += 1
@@ -211,12 +183,15 @@ class FetchFM:
         return result[:NUM_TOP_TRACKS]
 
     def _parse_top_artists(self, tracks):
-        d = defaultdict(int)
+        count = {}
         for track in tracks:
-            name = track['artist']
-            if name:
-                d[name] += 1
-        result = sorted(d.iteritems(), key=itemgetter(1), reverse=True)
+            name = track['artist']['name']
+            if name in count:
+                count[name]['count'] += 1
+            else:
+                count[name] = {'count': 1, 'artist': track['artist']}
+        result = sorted(count.itervalues(), key=itemgetter('count'),
+                        reverse=True)
         return result[:NUM_TOP_ARTISTS]
 
     def _parse_top_albums(self, tracks):
@@ -227,3 +202,24 @@ class FetchFM:
                 d[album] += 1
         result = sorted(d.iteritems(), key=itemgetter(1), reverse=True)
         return result[:NUM_TOP_ALBUMS]
+
+    def _do_query(self, query):
+        result = None
+        try:
+            # Create an API Request
+            url = self.API_URL + "?" + urllib.urlencode(query)
+
+            # Send Request and Collect it
+            data = urllib2.urlopen(url)
+
+            # Print it
+            result = json.load(data)
+
+            #Close connection
+            data.close()
+        except urllib2.HTTPError, e:
+            print "HTTP error: %d" % e.code
+        except urllib2.URLError, e:
+            print "Network error: %s" % e.reason.args[1]
+
+        return result
